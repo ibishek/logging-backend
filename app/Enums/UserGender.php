47bfed9ccca83f13ce027f3566cabfa38b4962c7
ibishek <abhishek.rijal1999@gmail.com>
2024-06-08 23:38:23 +0545

    fix(user gender and status enums): change way to provide details

--- app/Enums/UserGender.php
+++ app/Enums/UserGender.php
@@ -17,4 +17,27 @@ enum UserGender: string
     {
         return array_map(fn (UserGender $gender) => $gender->value, self::cases());
     }
+
+    public static function gender(int|string|null $gender): int|string|null
+    {
+        if (gettype($gender) === 'string') {
+            return match ($gender) {
+                UserGender::MALE->value => 1,
+                UserGender::FEMALE->value => 2,
+                UserGender::OTHER->value => 3,
+                default => null,
+            };
+        }
+
+        if (gettype($gender) === 'integer') {
+            return match ($gender) {
+                1 => UserGender::MALE->value,
+                2 => UserGender::FEMALE->value,
+                3 => UserGender::OTHER->value,
+                default => null,
+            };
+        }
+
+        return null;
+    }
 }
--- app/Enums/UserMaritalStatus.php
+++ app/Enums/UserMaritalStatus.php
@@ -16,4 +16,25 @@ enum UserMaritalStatus: string
     {
         return array_map(fn (UserMaritalStatus $status) => $status->value, self::cases());
     }
+
+    public static function maritalStatus(int|string|null $status): string|int|null
+    {
+        if (gettype($status) === 'integer') {
+            return match ($status) {
+                1 => UserMaritalStatus::UNMARRIED->value,
+                2 => UserMaritalStatus::MARRIED->value,
+                default => null
+            };
+        }
+
+        if (gettype($status) === 'string') {
+            return match ($status) {
+                UserMaritalStatus::UNMARRIED->value => 1,
+                UserMaritalStatus::MARRIED->value => 2,
+                default => null
+            };
+        }
+
+        return null;
+    }
 }
